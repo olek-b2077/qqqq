74aa3bd204b836ce0ea50f2cc7a6e6cdd1fd4820
olek-b2077 <oleksijborovik2010@gmail.com>
2024-08-02 16:55:06 +0300

    code

---
+++ extrim.py
@@ -0,0 +1,11 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+
+win = QWidget()
+win.resize(800,600)
+win.setWindowTitle("0w0 0_0 ")
+
+win.show()
+app.exec_()
